c9bfe5a94f5831a8988008efd65d039e50d0f3b2
Damien MARIE <damien@dam.io>
2014-03-29 19:17:34 -0400

    Updated links to link to the real content [but no content for now]

--- publicdb/urls.py
+++ publicdb/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout', { 'next_page':"/" }),
+    url('^login/$', 'datasets.views.nope', name="login"),
     url(r'^admin/', include(admin.site.urls)),
 )
 
@@ -13,7 +14,7 @@ urlpatterns += patterns('datasets.views',
     url(r'^new/$', 'new_api', name="new_api"),
     url(r'^list/$', 'api_list', name="api_list"),
 
-    url(r'^user/(?P<username>\w+)/$', 'nope', name='user_profile'), 
+    url(r'^user/(?P<pk>\d+)/$', 'nope', name='user_profile'), 
     url(r'^(?P<api_slug>\w+)/', include(patterns('datasets.views',
         url(r'^$', 'view_api', name="view_api"),
         url(r'^edit/$', 'edit_api', name="edit_api"),
@@ -22,6 +23,7 @@ urlpatterns += patterns('datasets.views',
         url(r'^class/(?P<class_slug>\w+)/', include(patterns('datasets.views',
             url(r'edit/$', 'nope', name="edit_klass"),
             url(r'delete/$', 'nope', name="delete_klass"),
+            url(r'instances/$', 'nope', name="instance_list"),
             url(r'instance/new/$', 'nope', name="new_instance"),
             url(r'instance/(?P<pk>\d>)/', include(patterns('datasets.views',
                 url(r'edit/$', 'nope', name="edit_instance"),
