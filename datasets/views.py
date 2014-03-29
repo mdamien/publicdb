c8974d0463030084542abb61e024f8817f63d79e
Damien MARIE <damien@dam.io>
2014-03-29 13:34:44 -0400

    Basic CRU[D] finished (list, create, update)

--- datasets/views.py
+++ datasets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, DetailView 
+from django.views.generic import TemplateView, CreateView, UpdateView, DetailView, ListView
 from django.core.urlresolvers import reverse
 
 from .models import API
@@ -7,11 +7,18 @@ from .models import API
 class HomeView(TemplateView):
     template_name = "home.html"
 
-class CreateAPIView(CreateView):
+class APICreateView(CreateView):
     model = API
     template_name = "datasets/new_api.html"
 
 class APIDetailView(DetailView):
     model = API
     template_name = "datasets/view_api.html"
-    context_obj = "api"
+
+class APIEditView(UpdateView):
+    model = API
+    template_name = "datasets/edit_api.html"
+
+class APIListView(ListView):
+    model = API
+    template_name = "datasets/list_apis.html"
--- publicdb/urls.py
+++ publicdb/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, include, url
 
-from datasets.views import HomeView, CreateAPIView, APIDetailView
+from datasets.views import HomeView, APICreateView, APIDetailView, APIEditView, APIListView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('', 
     url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^new/$', CreateAPIView.as_view(), name="new_api"),
+    url(r'^new/$', APICreateView.as_view(), name="new_api"),
+    url(r'^list/', APIListView.as_view(), name="api_list"),
     url(r'^(?P<slug>\w+)/infos', APIDetailView.as_view(), name="view_api"),
+    url(r'^(?P<slug>\w+)/edit', APIEditView.as_view(), name="edit_api"),
 
     url(r'^logout/$', 'django.contrib.auth.views.logout', { 'next_page':"/" }),
     url(r'^admin/', include(admin.site.urls)),
