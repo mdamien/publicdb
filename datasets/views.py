225d46ebef63655252b4f6f50d2854f2de93db98
Damien MARIE <damien@dam.io>
2014-03-29 18:18:50 -0400

    Make the urls.py more readeable by using plain views

--- datasets/views.py
+++ datasets/views.py
@@ -30,6 +30,8 @@ class APICreateView(CreateView):
         form.meta = "{}"
         return super(APICreateView, self).form_valid(form)
 
+new_api = APICreateView.as_view()
+
 class APIEditView(UpdateView):
     form_class = APIForm
     model = API
@@ -40,17 +42,21 @@ class APIEditView(UpdateView):
         form.meta = "{}"
         return super(APIEditView, self).form_valid(form)
 
+edit_api = APIEditView.as_view()
+
 class APIDetailView(DetailView):
     model = API
     template_name = "datasets/view_api.html"
 
+view_api = APIDetailView.as_view()
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
+home = HomeView.as_view()
 
 class APIListView(ListView):
     model = API
     template_name = "datasets/list_apis.html"
 
-
+api_list = APIListView.as_view()
--- publicdb/urls.py
+++ publicdb/urls.py
@@ -1,21 +1,23 @@
 from django.conf.urls import patterns, include, url
 
-from datasets.views import HomeView, APICreateView, APIDetailView, APIEditView, APIListView
-
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('', 
-    url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^new/$', APICreateView.as_view(), name="new_api"),
-    url(r'^list/', APIListView.as_view(), name="api_list"),
+urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout', { 'next_page':"/" }),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('datasets.views', 
+    url(r'^$', 'home', name="home"),
+    url(r'^new/$', 'new_api', name="new_api"),
+    url(r'^list/', 'api_list', name="api_list"),
 
     #TODO user profile user/<username>/ with api list
-    url(r'^(?P<slug>\w+)/', include(patterns('',
-        url(r'^$', APIDetailView.as_view(), name="view_api"),
-        url(r'^edit/', APIEditView.as_view(), name="edit_api"),
+    url(r'^(?P<slug>\w+)/', include(patterns('datasets.views',
+        url(r'^$', 'view_api', name="view_api"),
+        url(r'^edit/', 'edit_api', name="edit_api"),
+        #TODO Delete API
         #TODO CRUD Klasses
         #TODO CRUB Instances
     ))),
