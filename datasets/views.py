10f5c9e6cc73125850672e7d8b6c06b84a8f7624
Damien MARIE <damien@dam.io>
2014-03-29 12:32:35 -0400

    Added the possibility to create an API

--- datasets/views.py
+++ datasets/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, CreateView 
+from django.core.urlresolvers import reverse
+
+from .models import API
 
 class HomeView(TemplateView):
-    template_name = "home.html" 
+    template_name = "home.html"
+
+class CreateAPIView(CreateView):
+    model = API
+    template_name = "datasets/new_api.html"
+
+    def get_succes_url():
+        return "/"
--- publicdb/urls.py
+++ publicdb/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from datasets.views import HomeView
+from datasets.views import HomeView, CreateAPIView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('', 
-    url(r'^', HomeView.as_view(), name="home"),
+    url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^new/$', CreateAPIView.as_view(), name="new_api"),
     url(r'^admin/', include(admin.site.urls)),
 )
